deee6938309dd8c940a8a2abd8c37d4c9f2d57b9
zoleg-oi <zoleg@mail.ru>
2024-08-07 00:24:51 +0300

    Add files via upload

---
+++ module_1_3.py
@@ -0,0 +1,11 @@
+# Динамическая типизация
+
+name = 'Олег'
+print('Name: ' + name)
+age = 53
+print("Age: " + str(age))
+age = age + 1
+print("New Age: " + str(age))
+print(age)
+is_student = True
+print('Is student ' + str(is_student))
